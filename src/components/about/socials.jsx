a4b83d477e1f5290d80086b66fa20e6d1c1858c3
Marcelo Garcia <marcelo.andres99.ma@gmail.com>
2024-01-17 16:01:27 -0600

    fix lint error

--- src/components/about/socials.jsx
+++ src/components/about/socials.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import {
-  faTwitter,
   faGithub,
   faLinkedin,
   faInstagram,
