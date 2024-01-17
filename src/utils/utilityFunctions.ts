b3f4378c86a6243dced9e4f360027b2cd80d185f
Marcelo Garcia <marcelo.andres99.ma@gmail.com>
2024-01-17 03:01:37 -0600

    add initial ui revamp

--- src/utils/utilityFunctions.ts
+++
@@ -1,6 +0,0 @@
-export const scrollTo = (section: string) => {
-  const elementToView = document.getElementById(section);
-  if (elementToView) {
-    elementToView.scrollIntoView({ behavior: 'smooth' });
-  }
-};
